0a9ee469282ecbfcfe48006bfcbcb3f49a741ed5
TheCowRox <88647310+charlielidstone@users.noreply.github.com>
2023-02-07 19:09:17 -0800

    added some more great effects!

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 $('nav a').hover(function() {
-    $(this).addClass('expand');
-  }, function() {
-    $(this).removeClass('expand');
-  });
+  $(this).addClass('expand');
+}, function() {
+  $(this).removeClass('expand');
+});
+
+
+
+let previousScroll = 0;
 
 window.onscroll = function() {
-  console.log(scrollY)
-  $('.about-section h1').css({'transform': `perspective(300px) rotateX(${-((scrollY/10)-67.9)}deg)`})
+  const currentScroll = window.scrollY;
+  console.log(currentScroll);
+
+  $('.about-section h1').css({'transform': `perspective(300px) rotateX(${-((currentScroll/10)-67.9)}deg)`})
+
+  const hiddenElements = document.getElementsByClassName('to-be-hidden');
+  for (let i = 0; i < hiddenElements.length; i++) {
+    if (isInViewport(hiddenElements[i])) {
+      setTimeout(() => {
+        hiddenElements[i].classList.remove('hidden');
+      }, 200);
+    } else if (!isInViewport(hiddenElements[i]) && currentScroll < previousScroll && currentScroll < hiddenElements[i].offsetTop) {
+      hiddenElements[i].classList.add('hidden');
+    }
+  }
+
+  previousScroll = currentScroll;
 };
   
+
+
+function isInViewport(element, margin = 100) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= -margin &&
+    rect.left >= -margin &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + margin &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) + margin
+  );
+}
+
+  
+
