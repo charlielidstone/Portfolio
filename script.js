e6938025e541841b1cf4979dd90e97d034b86dea
TheCowRox <88647310+charlielidstone@users.noreply.github.com>
2023-02-07 21:57:35 -0800

    added some awesome functionality for reusable effects!

--- script.js
+++ script.js
@@ -4,24 +4,72 @@ $('nav a').hover(function() {
   $(this).removeClass('expand');
 });
 
+const effects = {
+  zoomIn: function(element) {
+    element.style.transform = 'scale(0.8)';
+  },
+  slideIn: function(element) {
+    element.style.transform = 'translateX(-20vw)';
+  },
+  zoomOut: function(element) {
+    element.style.transform = 'scale(1.2)'
+  }
+};
+
+function applyEffect(effectName) {
+  const elements = document.getElementsByClassName(effectName);
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add('dynamic')
+    effects[effectName](elements[i]);
+  }
+}
+
+const effectNames = Object.getOwnPropertyNames(effects);
+effectNames.forEach(effectName => {
+  applyEffect(effectName)
+})
+
+const behaviors = {
+  rotate: function(element) {
+    element.style.transform = `perspective(300px) rotateX(${-((window.scrollY/10)-element.offsetTop+(window.innerHeight/2.5))}deg)`
+  }
+}
+
+function applyBehavior(behaviorName) {
+  const elements = document.getElementsByClassName(behaviorName);
+  for (let i = 0; i < elements.length; i++) {
+    behaviors[behaviorName](elements[i]);
+  }
+}
+
+// const behaviorNames = Object.getOwnPropertyNames(effects);
+// behaviorNames.forEach(behaviorName => {
+//   applyBehavior(behaviorName)
+// })
 
 
 let previousScroll = 0;
 
 window.onscroll = function() {
   const currentScroll = window.scrollY;
-  console.log(currentScroll);
 
-  $('.about-section h1').css({'transform': `perspective(300px) rotateX(${-((currentScroll/10)-67.9)}deg)`})
+  const behaviorNames = Object.getOwnPropertyNames(behaviors);
+  behaviorNames.forEach(behaviorName => {
+      applyBehavior(behaviorName)
+  })
 
-  const hiddenElements = document.getElementsByClassName('to-be-hidden');
-  for (let i = 0; i < hiddenElements.length; i++) {
-    if (isInViewport(hiddenElements[i])) {
+  // $('.about-section h1').css({'transform': `perspective(300px) rotateX(${-((currentScroll/10)-67.9)}deg)`})
+
+  const dynamicElements = document.getElementsByClassName('dynamic');
+  for (let i = 0; i < dynamicElements.length; i++) {
+    if (isInViewport(dynamicElements[i])) {
       setTimeout(() => {
-        hiddenElements[i].classList.remove('hidden');
+        dynamicElements[i].classList.remove('hidden');
+        dynamicElements[i].classList.add('visible');
       }, 200);
-    } else if (!isInViewport(hiddenElements[i]) && currentScroll < previousScroll && currentScroll < hiddenElements[i].offsetTop) {
-      hiddenElements[i].classList.add('hidden');
+    } else if (!isInViewport(dynamicElements[i]) && currentScroll < previousScroll && currentScroll < dynamicElements[i].offsetTop) {
+      dynamicElements[i].classList.add('hidden');
+      dynamicElements[i].classList.remove('visible');
     }
   }
 
@@ -34,9 +82,7 @@ function isInViewport(element, margin = 100) {
   const rect = element.getBoundingClientRect();
   return (
     rect.top >= -margin &&
-    rect.left >= -margin &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + margin &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth) + margin
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + margin
   );
 }
 
