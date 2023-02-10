33d319fec100a65e0502b82deb4ca9c988a6572b
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2023-02-10 13:52:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ const behaviors = {
     const maxDistance = window.innerHeight / 2 + elementHeight / 2;
     const scale = 1 * (1 - distance / maxDistance);
     // element.style.transform = ` scale(${scale})`;
-    element.style.transform = `scale(${scale}) perspective(300px) rotateX(${-((window.scrollY/20)-element.offsetTop+(window.innerHeight/2.2))}deg)`
+    element.style.transform = `scale(${scale}) perspective(300px) rotateX(${-((window.scrollY/20)-element.offsetTop+(window.innerHeight/2))}deg)`
   }
 }
 
