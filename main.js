9cdaf50c177c579d67ac09943630dd9eb4a94149
TheCowRox <88647310+charlielidstone@users.noreply.github.com>
2023-07-25 21:41:18 -0700

    created cool thing

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import { gsap } from "gsap";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// gsap.from("#main__title", {
+//   duration: 2, 
+//   filter: "blur(1em)", 
+//   ease: "ease-out",
+// });
 
-setupCounter(document.querySelector('#counter'))
+var mainTitle = document.getElementById("main__title");
+const fonts = ['VT323', 'Lisu Bosa', 'Caveat', 'Chokokutai', 'Handjet', 'Lugrasimo', 'Lumanosimo', 'Permanent Marker', 'Rowdies', 'Zeyada', 'Big Shoulders Inline Text', 'Press Start 2P', 'Kablammo', 'Moirai One', 'Monoton'];
+
+applyFontSwapAnim(mainTitle, fonts, "VT323", 2000, 200);
+
+function applyFontSwapAnim(element, fonts, defaultFont, duration = 2000, interval = 200) {
+  const text = element.innerText;
+  const chars = text.split("");
+
+  let totalTime = 0;
+
+  const charElements = [];
+  
+  chars.forEach((char, index) => {
+    charElements.push(document.createElement("span"));
+    charElements[index].style.display = "inline-block";
+    charElements[index].style.width = "0.8em";
+    charElements[index].innerText = char;
+  });
+``
+  const toClear = setInterval(() => {
+    if (totalTime >= duration) {
+      clearInterval(toClear);
+      element.innerHTML = "";
+      charElements.forEach((charElement) => {
+        charElement.style.fontFamily = "monospace";
+        element.innerHTML += charElement.outerHTML;
+      });
+      return;
+    }
+
+    element.innerHTML = ""
+    
+    charElements.forEach((charElement, index) => {
+      const randomFontIndex = Math.floor(Math.random() * fonts.length);
+      charElement.style.fontFamily = fonts[randomFontIndex];
+      element.innerHTML += charElement.outerHTML;
+    });
+
+    totalTime += interval;
+  }, interval);
+}
+
+
