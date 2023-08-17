0b24a3c691ddc01be4163ce2f28346bbdd50f65b
TheCowRox <88647310+charlielidstone@users.noreply.github.com>
2023-08-16 17:21:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import mainTitleAnimation from "./scripts/mainTitle__Animation.js";
 import gsapEffects from "./scripts/gsapEffects.js";
-import threeEffects from "./scripts/threeEffects.js";
+// import threeEffects from "./scripts/threeEffects.js";
 
 gsapEffects.setup();
 mainTitleAnimation.run();
-threeEffects.animate();
+// threeEffects.animate();
