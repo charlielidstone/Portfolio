86403750619102d2acd04f7aaf90fb4c66a0e743
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2023-12-21 19:07:58 -0800

    Refactor gsapEffects.js and update style.css

--- scripts/gsapEffects.js
+++ scripts/gsapEffects.js
@@ -1,106 +1,89 @@
-let mainTitle = () => {
-  gsap.to("#main__title", {
-    filter: "blur(7px)",
-    scale: 0.9,
-    duration: 1,
-    ease: "power2.out",
-    scrollTrigger: {
-      trigger: "header",
-      start: "bottom bottom",
-      end: "bottom top",
-      scrub: 1,
-      toggleActions: "restart pause resume pause"
-    },
-  });
+const gsapEffects = {
+  setup: function() {
+    gsap.to("#main__title", {
+      filter: "blur(7px)",
+      scale: 0.9,
+      duration: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: "header",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: 1,
+        toggleActions: "restart pause resume pause"
+      },
+    });
 
-  const sectionH1s = document.querySelectorAll('section h1');
-  const sections = document.querySelectorAll('section');
-  sectionH1s.forEach((sectionH1, index) => {
+    const sectionH1s = document.querySelectorAll('section h1');
+    const sections = document.querySelectorAll('section');
+    sectionH1s.forEach((sectionH1, index) => {
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: sections[index],
+          start: "center center",
+          end: "bottom top",
+          scrub: true,
+          pin: true,
+        },
+      });
+
+      timeline
+        .to(sectionH1, { x: 0, duration: 1 })
+        .to(sectionH1, { x: 0, duration: 1, delay: 2 });
+    });
+
+    const signaturePath = document.getElementById('signature-path');
+    const signatureLength = Math.ceil(signaturePath.getTotalLength());
+    signaturePath.style.strokeDasharray = signatureLength;
+    const signatureContainer = document.getElementById('signature-container');
     const timeline = gsap.timeline({
       scrollTrigger: {
-        trigger: sections[index],
+        trigger: signatureContainer,
         start: "center center",
-        end: "bottom top",
-        scrub: true,
+        end: "bottom -100%",
+        scrub: 0.5,
         pin: true,
+        pinSpacing: true,
       },
     });
 
     timeline
-      .to(sectionH1, { x: 0, duration: 1 })
-      .to(sectionH1, { x: 0, duration: 1, delay: 2 });
-  });
-}
+      .from(signaturePath, { strokeDashoffset: signatureLength, duration: 2 })
+      .to(signatureContainer, { opacity: 0, filter: "blur(7px)", translateY: "-100px", duration: 1})
 
-let signature = () => {
-  const signaturePath = document.getElementById('signature-path');
-  const signatureLength = Math.ceil(signaturePath.getTotalLength());
-  signaturePath.style.strokeDasharray = signatureLength;
-  const signatureContainer = document.getElementById('signature-container');
-  const timeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: signatureContainer,
-      start: "center center",
-      end: "bottom -100%",
-      scrub: 0.5,
-      pin: true,
-      pinSpacing: true,
-    },
-  });
+    const helloWorld = document.getElementById('hello-world');
+    const worksSection = document.getElementById('works__section');
 
-  timeline
-    .from(signaturePath, { strokeDashoffset: signatureLength, duration: 2 })
-    .to(signatureContainer, { opacity: 0, filter: "blur(7px)", translateY: "-100px", duration: 1})
+    gsap.to(helloWorld, {
+      scale: 0.9,
 
-}
-
-let helloWorld = () => {
-  const helloWorld = document.getElementById('hello-world');
-    
-  gsap.to(helloWorld, {
-    scale: 0.9,
-    opacity: 0,
-
-    scrollTrigger: {
-      trigger: helloWorld,
-      start: "center center",
-      end: "bottom top",
-      pin: true,
-      scrub: true,
-      toggleActions: "restart none none reverse",
-    },
-  });
-}
-
-let worksSection = () => {
-  const worksSection = document.getElementById('works__section');
-
-  gsap.to(worksSection, {
-    scale: 0.8,
-    borderRadius: "50px",
-    y: "30px",
-
-    scrollTrigger: {
-      trigger: worksSection,
-      start: "top top",
-      end: "bottom top",
-      scrub: true,
-      pin: true,
-      toggleActions: "restart none none reverse",
-      markers: true,
-    },
-  });
-}
+      scrollTrigger: {
+        trigger: worksSection,
+        start: "top bottom",
+        end: "bottom top",
+        pin: helloWorld,
+        scrub: true,
+        toggleActions: "restart none none reverse",
+        markers: true,
+      },
+    });
 
 
-const gsapEffects = {
-  setup: function() {
+    gsap.to(worksSection, {
+      scale: 0.8,
+      borderRadius: "50px",
+      y: "10px",
 
-    mainTitle();
-    signature();
-    helloWorld();
-    worksSection();
-
+      scrollTrigger: {
+        trigger: worksSection,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        pin: true,
+        toggleActions: "restart none none reverse",
+        markers: true,
+      },
+    });
   }
 };
 
