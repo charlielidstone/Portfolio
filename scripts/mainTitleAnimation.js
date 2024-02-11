de91805f7ef6d6ab0d04cda2fab49bb1b8ab16b3
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2024-02-10 20:38:06 -0800

    import js only fonts in js file

--- scripts/mainTitleAnimation.js
+++ scripts/mainTitleAnimation.js
@@ -1,51 +1,65 @@
-const c = {
-  defaultFont: "Old London",
-  defaultFontSize: "1em",
-  defaultPositionTop: "0em",
-  defaultPositionLeft: "0.2em",
-}
+const oldLondonFont = new FontFace('Old London', 'url(public/fonts/old_london/OldLondon.ttf)');
+const usAngelFont = new FontFace('US Angel', 'url(public/fonts/usangel/usangel.ttf)');
+const arb85Font = new FontFace('ARB85', 'url(public/fonts/arb85-2/arb85.ttf)');
+const cloisterBlackFont = new FontFace('Cloister Black', 'url(public/fonts/cloister-black/CloisterBlack.ttf)');
 
-const h = {
-  defaultFont: "Nova Flat",
-  defaultFontSize: "0.9em",
-  defaultPositionTop: "0em",
-  defaultPositionLeft: "0em",
-}
+// Load all the fonts
+Promise.all([
+  oldLondonFont.load(),
+  usAngelFont.load(),
+  arb85Font.load(),
+  cloisterBlackFont.load()
+]).then((loadedFonts) => {
+  // Add the loaded fonts to the document
+  loadedFonts.forEach((font) => {
+    document.fonts.add(font);
+  });
+});
 
-const a = {
-  defaultFont: "UnifrakturCook",
-  defaultFontSize: "1em",
-  defaultPositionTop: "0em",
-  defaultPositionLeft: "-0.1em",
-}
-
-const r = {
-  defaultFont: '"ARB 85 Poster Script JAN-39"',
-  defaultFontSize: "0.9em",
-  defaultPositionTop: "0em",
-  defaultPositionLeft: "-0.1em",
-}
-
-const l = {
-  defaultFont: "US Angel",
-  defaultFontSize: "1em",
-  defaultPositionTop: "0em",
-  defaultPositionLeft: "0.2em",
-}
-
-const i = {
-  defaultFont: "Grenze Gotisch",
-  defaultFontSize: "1.1em",
-  defaultPositionTop: "-0.06em",
-  defaultPositionLeft: "0.1em",
-}
-
-const e = {
-  defaultFont: "Patua One",
-  defaultFontSize: "1em",
-  defaultPositionTop: "0em",
-  defaultPositionLeft: "-0.2em",
-}
+const MainLogo = {
+  c: {
+    defaultFont: "Old London",
+    defaultFontSize: "1em",
+    defaultPositionTop: "0em",
+    defaultPositionLeft: "0.2em",
+  },
+  h: {
+    defaultFont: "Nova Flat",
+    defaultFontSize: "0.9em",
+    defaultPositionTop: "0em",
+    defaultPositionLeft: "0em",
+  },
+  a: {
+    defaultFont: "UnifrakturCook",
+    defaultFontSize: "1em",
+    defaultPositionTop: "0em",
+    defaultPositionLeft: "-0.1em",
+  },
+  r: {
+    defaultFont: '"ARB85"',
+    defaultFontSize: "0.9em",
+    defaultPositionTop: "0em",
+    defaultPositionLeft: "-0.1em",
+  },
+  l: {
+    defaultFont: "US Angel",
+    defaultFontSize: "1em",
+    defaultPositionTop: "0em",
+    defaultPositionLeft: "0.2em",
+  },
+  i: {
+    defaultFont: "Grenze Gotisch",
+    defaultFontSize: "1.1em",
+    defaultPositionTop: "-0.06em",
+    defaultPositionLeft: "0.1em",
+  },
+  e: {
+    defaultFont: "Patua One",
+    defaultFontSize: "1em",
+    defaultPositionTop: "0em",
+    defaultPositionLeft: "-0.2em",
+  },
+};
 
 const mainTitleAnimation = {
   run: function() {
@@ -62,7 +76,7 @@ const mainTitleAnimation = {
         '"UnifrakturCook"',
         'Old London',
         // 'Cutive Mono',
-        // '"ARB 85 Poster Script JAN-39"',
+        // '"ARB85"',
       ];
       const mainTitleAnimationDuration = 4300; // 4300ms works well
 
@@ -112,7 +126,7 @@ function applyFontSwapAnimation(element, fonts, defaultFont, duration = 2000, in
         charElement.style.fontFamily = defaultFont;        if (index === 0) { charElement.style.fontFamily = 'Old London'; charElement.style.left = '0.2em' }
         else if (index === 1) { charElement.style.fontFamily = 'Nova Flat'; charElement.style.fontSize = '0.9em' }
         else if (index === 2) { charElement.style.fontFamily = 'UnifrakturCook'; charElement.style.left = '-0.1em' }
-        else if (index === 3) { charElement.style.fontFamily = '"ARB 85 Poster Script JAN-39"'; charElement.style.fontSize = '0.9em'; charElement.style.left = '-0.1em' }
+        else if (index === 3) { charElement.style.fontFamily = '"ARB85"'; charElement.style.fontSize = '0.9em'; charElement.style.left = '-0.1em' }
         else if (index === 4) { charElement.style.fontFamily = 'US Angel'; charElement.style.left = '0.2em' }
         else if (index === 5) { charElement.style.fontFamily = 'Grenze Gotisch'; charElement.style.fontSize = '1.1em'; charElement.style.left = '0.1em'; charElement.style.top = '-0.06em' }
         else if (index === 6) { charElement.style.fontFamily = 'Patua One'; charElement.style.left = '-0.2em' }
@@ -141,13 +155,13 @@ function applyFontSwapAnimation(element, fonts, defaultFont, duration = 2000, in
         });
   
         charElement.addEventListener("mouseout", () => {
-          if (charElement.innerHTML == "C") charElement.style.fontFamily = c.defaultFont;
-          else if (charElement.innerHTML == "H") charElement.style.fontFamily = h.defaultFont;
-          else if (charElement.innerHTML == "A") charElement.style.fontFamily = a.defaultFont;
-          else if (charElement.innerHTML == "R") charElement.style.fontFamily = r.defaultFont;
-          else if (charElement.innerHTML == "L") charElement.style.fontFamily = l.defaultFont;
-          else if (charElement.innerHTML == "I") charElement.style.fontFamily = i.defaultFont;
-          else if (charElement.innerHTML == "E") charElement.style.fontFamily = e.defaultFont;
+          if (charElement.innerHTML == "C") charElement.style.fontFamily = MainLogo.c.defaultFont;
+          else if (charElement.innerHTML == "H") charElement.style.fontFamily = MainLogo.h.defaultFont;
+          else if (charElement.innerHTML == "A") charElement.style.fontFamily = MainLogo.a.defaultFont;
+          else if (charElement.innerHTML == "R") charElement.style.fontFamily = MainLogo.r.defaultFont;
+          else if (charElement.innerHTML == "L") charElement.style.fontFamily = MainLogo.l.defaultFont;
+          else if (charElement.innerHTML == "I") charElement.style.fontFamily = MainLogo.i.defaultFont;
+          else if (charElement.innerHTML == "E") charElement.style.fontFamily = MainLogo.e.defaultFont;
           else charElement.style.fontFamily = defaultFont;
           clearInterval(singleCharToClear);
         });
