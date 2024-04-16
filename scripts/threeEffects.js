f16f04eb6027ea753ac02123e236af45dd23323f
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2024-04-15 21:22:57 -0700

    Update threeEffects.js

--- scripts/threeEffects.js
+++ scripts/threeEffects.js
@@ -3,7 +3,6 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const threeEffects = (function() {
-
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({
