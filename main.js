7723f064ab48cd20f75e5d27876c60138a7297de
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2026-02-03 15:15:53 -0800

    add chromatic aberation type stuff

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const body = document.body;
+
 const box = document.getElementById('box');
 
 let offsetX, offsetY;
@@ -19,3 +21,22 @@ box.addEventListener("pointermove", (e) => {
 box.addEventListener("pointerup", () => {
     isDragging = false;
 });
+
+const ca = document.querySelector('.chromatic-aberration div');
+
+// make box-shadow point in direction of mouse relative to center of element, between 1px and -1px
+body.addEventListener("pointermove", (e) => {
+    const rect = ca.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const offsetX = e.clientX - centerX;
+    const offsetY = e.clientY - centerY;
+    const shadowX = offsetX / 20;
+    const shadowY = offsetY / 20;
+    ca.style.textShadow = `
+        ${shadowX}px ${shadowY}px 0 rgba(255,255,0,0.1),
+        ${shadowX * -1}px ${shadowY * -1}px 0 rgba(0,255,255,0.1)
+    `;
+});
+
+
