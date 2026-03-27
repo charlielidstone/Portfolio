4044a8dba1fa1045453d60cf4ffde8b1bd4966fe
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2026-03-27 13:03:16 -0700

    i dunno

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import particles from "./modules/particles.js";
 import initVariableWidthText from "./modules/variableWidthText.js";
+import mainTitleAnimation from "./modules/mainTitleAnimation.js";
+
 
 particles.render();
 initVariableWidthText();
+mainTitleAnimation.run();
