acf108cb2d18f91d08e210837a78f7c13e808ffd
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2026-03-28 23:55:43 -0700

    tryna get this gsap to work

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-import particles from "./modules/particles.js";
-import initVariableWidthText from "./modules/variableWidthText.js";
-import mainTitleAnimation from "./modules/mainTitleAnimation.js";
-
+// import particles from "./modules/particles.js";
+// import initVariableWidthText from "./modules/variableWidthText.js";
+// import mainTitleAnimation from "./modules/mainTitleAnimation.js";
+import heroSectionScroll from "./modules/heroSectionScroll.js";
 import { applyDragability } from "./scripts/draggable.js";
 
-const box = document.getElementById('box');
-applyDragability(box);
+// const box = document.getElementById('box');
+// applyDragability(box);
 
-particles.render();
-initVariableWidthText();
-mainTitleAnimation.run();
+// particles.render();
+
+// initVariableWidthText();
+
+// mainTitleAnimation.run();
+
+heroSectionScroll.setup();
