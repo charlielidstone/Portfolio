ea5e1b3c79669a9192f30051f522c8e29102bf7c
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2026-04-08 17:54:12 -0700

    fixed rotation of project cards

--- modules/projectsSectionScroll.js
+++ modules/projectsSectionScroll.js
@@ -7,9 +7,9 @@ const projectsSectionScroll = {
   setup: function() {
     const tl = gsap.timeline();
 
-    tl.from("#p1", { rotate: "3deg" }).to("#p1", { translate: "800px 200px", rotate: "10deg" })
-    .from("#p2", { rotate: "-4deg" }).to("#p2", { translateX: "-800px 200px", rotate: "-7deg" })
-    .from("#p3", { rotate: "2deg" }).to("#p3", { translateX: "1000px", rotate: "-2deg" })
+    tl.from("#p1", { rotate: "0deg" }).to("#p1", { translate: "40vw 10vw", rotate: "10deg" })
+    .from("#p2", { rotate: "-4deg" }).to("#p2", { translate: "-40vw 0", rotate: "-7deg" })
+    .from("#p3", { rotate: "2deg" }).to("#p3", { translate: "40vw -5vw", rotate: "-2deg" })
 
     ScrollTrigger.create({
         animation: tl,
