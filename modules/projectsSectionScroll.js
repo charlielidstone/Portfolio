7bf7a64bd6cbfd723c3b2388ea9b65a6b8bfeaca
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2026-04-09 13:55:01 -0700

    fix imports

--- modules/projectsSectionScroll.js
+++ modules/projectsSectionScroll.js
@@ -10,6 +10,7 @@ const projectsSectionScroll = {
     tl.from("#p1", { rotate: "0deg" }).to("#p1", { translate: "40vw 10vw", rotate: "10deg" })
     .from("#p2", { rotate: "-4deg" }).to("#p2", { translate: "-40vw 0", rotate: "-7deg" })
     .from("#p3", { rotate: "2deg" }).to("#p3", { translate: "40vw -5vw", rotate: "-2deg" })
+    .to("#p4", { zIndex: 4 }).to("#p4", { scale: 4 })
 
     ScrollTrigger.create({
         animation: tl,
