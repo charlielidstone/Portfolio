e9114b62bc0dd3eedc614be963794f47926603a2
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2026-04-23 21:15:00 -0700

    fix imports

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-import particles from "./modules/particles.js";
-import initVariableWidthText from "./modules/variableWidthText.js";
 import mainTitleAnimation from "./modules/mainTitleAnimation.js";
 import heroSectionScroll from "./modules/heroSectionScroll.js";
 import projectsSectionScroll from "./modules/projectsSectionScroll.js";
-import { applyDragability } from "./scripts/draggable.js";
-// import threeEffects from "./scripts/threeEffects.js";
 import smoothScrolling from "./modules/smoothScrolling.js";
+
+// import particles from "./modules/particles.js";
+// import initVariableWidthText from "./modules/variableWidthText.js";
+// import { applyDragability } from "./scripts/draggable.js";
+// import threeEffects from "./scripts/threeEffects.js";
 
 import { gsap } from "gsap";
 
 import { CustomEase } from "gsap/CustomEase";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
-
 
 // import { gsap } from "gsap";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
--- scripts/threeEffects.js
+++ scripts/threeEffects.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const threeEffects = (function() {
     const scene = new THREE.Scene();
